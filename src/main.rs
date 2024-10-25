1a43d03f893151a0a6b8ebf9d4e7acb6a5da15df
Jan Vanbuel <janvanbuel@hotmail.com>
2024-10-25 16:54:10 +0200

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
+use std::{fs, panic};
+
 use leptos::*;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct QueryEngine {
+    id: String,
+    short_name: String,
+    description: String,
+    url: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Integration {
+    id: String,
+    short_name: String,
+    description: String,
+}
+
+const ENGINES: &str = include_str!("../query_engines.yaml");
+const INTEGRATIONS: &str = include_str!("../integrations.yaml");
 
 fn main() {
-    mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    let query_engines: Vec<QueryEngine> =
+        serde_yaml::from_str(ENGINES).expect("Unable to parse YAML");
+
+    let integrations: Vec<Integration> =
+        serde_yaml::from_str(INTEGRATIONS).expect("Unable to parse YAML");
+
+    // let debug_qe = format!("{:?}", query_engines);
+
+    mount_to_body(move || view! { <p>{format!("{:?}", query_engines)}</p> })
 }
